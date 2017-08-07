09e744e68cb9bb57b8418c30eaabebaa08cace44
Sukohi Kuhoh <capilano.sukohi@gmail.com>
2017-08-08 04:48:57 +0900

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # x-editable-file
-A plugin for x-editable that manages file.
+A plugin for x-editable that manages file.  
+
+This package is inspired by [this page](https://github.com/vitalets/x-editable/issues/50).  
+Thank you, marciocamello, lloydzhou and disq!
+
+# Installation
+
+    bower install x-editable-file
+    
+# Usage
+
+Please see `examples` folder.
+
+
+# License
+
+This package is licensed under the MIT License.
+
+Copyright 2017 Sukohi Kuhoh
---
+++ x-editable-file.js
@@ -0,0 +1,194 @@
+/**
+ File editable input.
+
+ @class file
+ @extends abstractinput
+ @experimental
+ @example
+ <a href="#" id="file" data-type="file" data-pk="1" data-value="filename.jpg"></a>
+
+<script>
+    $('#file').editable({
+        url: '/post',
+        type: 'file',
+        pk: 1,
+        inputclass: '',
+        method: 'POST',
+        name: 'file',
+        buttonLabel: 'Chose file',
+        maxFilenameLength: 50,
+        allowDelete: true,
+        deleteLabel: 'Delete'
+    });
+</script>
+ **/
+
+(function ($) {
+
+    "use strict";
+
+    var File = function (options) {
+        this.init('file', options, File.defaults);
+    };
+
+    //inherit from Abstract input
+    $.fn.editableutils.inherit(File, $.fn.editabletypes.abstractinput);
+
+    $.extend(File.prototype, {
+        render: function() {
+            var self = this;
+            this.$input = this.$tpl.find('input');
+
+            this.$input.filter('[name="file"]').bind('change focus click', function() {
+                var filename = self.getFilename($(this).val());
+                self.setFilename(filename);
+                self.fileSelected = true;
+            });
+            this.$tpl.find('button').text(this.options.buttonLabel);
+
+            if(this.options.allowDelete) {
+
+                this.$tpl.parent().append(this.getDeleteHtml());
+
+            }
+
+            var defaultFilename = $(this.options.scope).data('value');
+
+            if($.fn.editableform.engine === 'bs3') {
+                this.$tpl.find('button').addClass('btn btn-default');
+            }
+
+            if(this.options.inputclass) {
+                this.$tpl.find('button').addClass(this.options.inputclass);
+            }
+        },
+        value2html: function(value, element) {
+            if(!value) {
+                $(element).empty();
+                return;
+            }
+
+            if(this.withDeleting) {
+
+                value = '';
+
+            }
+
+            $(element).html(value);
+        },
+        value2input: function(value) {
+            if(!value) {
+                return;
+            }
+            this.$input.filter('[name="file"]').val(value.file);
+            this.$input.filter('[name="caption"]').val(value.caption);
+            this.$input.filter('[name="url"]').val(value.url);
+        },
+        input2value: function() {
+            return this.getFullFilename(this.$input.filter('[name="file"]').val());
+        },
+        activate: function() {
+
+            var self = this;
+            this.withDeleting = false;
+            $(this.options.scope).editable('option', 'ajaxOptions', {
+                dataType: 'json',
+                contentType: false,
+                processData: false,
+                type: this.options.method,
+                success: function() {
+
+                    self.fileSelected = false;
+
+                }
+            });
+            var inputName =  this.options.name;
+            $(this.options.scope).editable('option', 'params', function(){
+
+                var form = self.$tpl.closest('form').get(0);
+                var formData = new FormData(form);
+                var deleteFlag = formData.get('delete');
+                formData.delete('file');
+
+                if(self.fileSelected && deleteFlag == null) {
+
+                    formData.append(inputName, self.$input.filter('[name="file"]')[0].files[0]);
+
+                }
+
+                if(deleteFlag) {
+
+                    self.withDeleting = true;
+
+                }
+
+                formData.append('pk', $(self.options.scope).data('pk'));
+                return formData;
+
+            });
+
+            this.$input.filter('[name="'+ inputName +'"]').focus();
+
+        },
+        getFullFilename: function(path) {
+
+            return path.split(/[\/\\]/).pop();
+
+        },
+        getFilename: function(path) {
+
+            var filename = this.getFullFilename(path);
+            var maxLength = this.options.maxFilenameLength;
+
+            if(filename.length > maxLength) {
+
+                filename = filename.substr(0, maxLength) +'..';
+
+            }
+
+            return filename;
+
+        },
+        getDeleteHtml: function() {
+
+            var label = this.options.deleteLabel;
+            return ['<div class="editable-delete-file">',
+                    '<label><input name="delete" type="checkbox" value="1"> '+ label +'</label>',
+                    '</div>'].join('');
+
+        },
+        setFilename: function(filename) {
+
+            if(filename) {
+
+                $(this.$tpl[1]).text(filename);
+
+            }
+
+        },
+        fileSelected: false,
+        withDeleting: false
+    });
+
+    File.defaults = $.extend({}, $.fn.editabletypes.abstractinput.defaults, {
+        tpl: [
+            '<div class="editable-file">',
+            '<span style="position:relative; display: inline-block; overflow: hidden; cursor: pointer;">',
+            '<input type="file" name="file" class="input-small" size="1" style="opacity: 0;filter: alpha(opacity=0); cursor: pointer; font-size: 400%; height: 600%; position: absolute; top: 0; right: 0; width: 240%" />',
+            '<button type="button" style="cursor: pointer; display: inline-block; margin-right: 5px;"></button>',
+            '</span></div>',
+            '<div class="editable-filename"></div>'
+        ].join(''),
+
+        inputclass: '',
+        method: 'POST',
+        name: 'file',
+        buttonLabel: 'Chose file',
+        maxFilenameLength: 50,
+        allowDelete: true,
+        deleteLabel: 'Delete'
+    });
+
+    $.fn.editabletypes.file = File;
+
+}(window.jQuery));
